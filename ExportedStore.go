a757cd4be571ec9eb7f06bf43d43520e8fd6a888
HasinduLanka <hlrathnayaka@gmail.com>
2022-03-26 20:49:45 +0530

    Exported funcs

--- ExportedStore.go
+++ ExportedStore.go
@@ -6,3 +6,15 @@ type ExportedStore struct {
 	Set  func(string)
 	Next func() string
 }
+
+func (es *ExportedStore) GetValue() string {
+	return es.Get()
+}
+
+func (es *ExportedStore) SetValue(s string) {
+	es.Set(s)
+}
+
+func (es *ExportedStore) NextValue() string {
+	return es.Next()
+}
